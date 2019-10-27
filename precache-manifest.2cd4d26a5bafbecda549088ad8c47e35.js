5dde96eb7a5cf45655a52e22e8c3ea22c9100d6c
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-10-27 20:52:51 +0100

    Updates

---
+++ precache-manifest.2cd4d26a5bafbecda549088ad8c47e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078c0b2b2ed515e2d911473048d66fb1",
+    "url": "/DadJokes/index.html"
+  },
+  {
+    "revision": "a26cc5ba2ad89211c2c3",
+    "url": "/DadJokes/static/css/main.93d915a8.chunk.css"
+  },
+  {
+    "revision": "61edf80b125bbada9360",
+    "url": "/DadJokes/static/js/2.e7857603.chunk.js"
+  },
+  {
+    "revision": "a26cc5ba2ad89211c2c3",
+    "url": "/DadJokes/static/js/main.750b10a6.chunk.js"
+  },
+  {
+    "revision": "6c617adf4f87a9801045",
+    "url": "/DadJokes/static/js/runtime-main.80c5de0a.js"
+  }
+]);
